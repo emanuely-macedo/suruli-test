7e3266214c56fd60f5953ca883ea754ccfb94cc6
Emanuely Macedo Padovan <emanuely.mpadovan@gmail.com>
2026-09-01 16:28:41 -0300

    edição dos ícones do TabNavigator

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,3 +1,4 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import ListaFilmes from "../screens/MoviesListScreen";
@@ -6,9 +7,42 @@ const Tab = createBottomTabNavigator()
 
 export default function BottomTabNavigator() {
     return (
-        <Tab.Navigator initialRouteName="Home">
-            <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Lista de Filmes" component={ListaFilmes}/>
+        <Tab.Navigator 
+            initialRouteName="Home"
+            screenOptions={{
+                tabBarActiveTintColor: "004c94",
+                tabBarInactiveTintColor: "7890a5",
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                }
+            }}
+        >
+            <Tab.Screen 
+                name="Home" 
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                            name="home"
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen 
+                name="Lista de Filmes" 
+                component={ListaFilmes}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                            name="film"
+                            size={size}
+                            color={color}
+                        />
+                    )
+                }}
+            />
         </Tab.Navigator>
     );
 }
